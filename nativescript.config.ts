d155c1ae87d52917a3a4099e3aec4939e8178821
cos4h <olvadis2004@gmail.com>
2024-10-29 09:06:40 -0500

    path full

--- nativescript.config.ts
+++ nativescript.config.ts
@@ -1,11 +1,12 @@
 import { NativeScriptConfig } from '@nativescript/core';
 
 export default {
-  id: 'org.nativescript.app',
-  appPath: 'src',
-  appResourcesPath: '/platforms',
+  id: "org.nativescript.app",
+  appPath: "src",
+  appResourcesPath:
+    "/home/runner/work/SistemaCostos/SistemaCostos/platforms/",
   android: {
-    v8Flags: '--expose_gc',
-    markingMode: 'none'
-  }
+    v8Flags: "--expose_gc",
+    markingMode: "none",
+  },
 } as NativeScriptConfig;
